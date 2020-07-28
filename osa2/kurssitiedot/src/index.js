cae7db45b209d822f842db0b8570f82b6145058f
alasz <antti.laszlo@gmail.com>
2020-07-28 18:43:27 +0300

    2.1 done

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.coursename}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  console.log("part props:", props)
+  return (
+    <>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  console.log(props.course.course.parts)
+  const parts = props.course.course.parts
+  console.log(parts)
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises}>
+          {/* {part.name} */}
+        </Part>
+      )}
+      {/* <Part part={props.part[0].name} exercises={props.part[0].exercises} />
+      <Part part={props.part[1].name} exercises={props.part[1].exercises} />
+      <Part part={props.part[2].name} exercises={props.part[2].exercises} /> */}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.part[0].exercises + props.part[1].exercises + props.part[2].exercises}</p>
+    </>
+  )
+}
+
+const Course = (props) => {
+  console.log("course props: ", props)
+  return (
+    <div>
+      <Header coursename={props.course.name} />
+      <Content course={props} />
+      {/* <Total part={props.parts} /> */}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
