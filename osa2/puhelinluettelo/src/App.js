2776e422ee15c1cb52c47d0a612fd6dcb6094827
alasz <antti.laszlo@gmail.com>
2020-09-15 12:07:37 +0300

    2.11 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12345' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('name')
   const [ newNumber, setNewNumber ] = useState('number')
   const [ filter, setFilter ] = useState('')
 
-
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter filter={filter} setFilter={setFilter}/>
-        
       <h2>Add new</h2>
       <PersonForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}/>
-     
       <h2>Numbers</h2>
       <Persons persons={persons} filter={filter}/>
-
     </div>
   )
-
 }
 
 export default App
