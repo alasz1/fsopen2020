e9194ba6622d64b61d3557dea9e34a218de5b429
alasz <antti.laszlo@gmail.com>
2020-10-23 14:45:17 +0300

    4.4 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum.reduce((a, b) => {
+            return a + b[item]
+        }, 0)
+    }
+    const total = reducer(blogs, 'likes')
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
